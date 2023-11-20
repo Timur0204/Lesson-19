280687f1beb65ea48e5fd3abf90a9720330dc7ba
Timur0204 <timur02042008@gmail.com>
2023-11-20 14:54:17 +0200

    fixed

--- assets/js/add2.js
+++ assets/js/add2.js
@@ -1,12 +1,9 @@
    let leapYear = +prompt('Введите год, чтобы проверить високосный он или нет');
    console.log(`Год: ${leapYear}`);
 
-   if(leapYear % 4 == 0  && leapYear % 400 == 0) {
+   if(leapYear % 4 == 0  && leapYear % 100 !== 0 || leapYear % 400 == 0) {
     console.log('Год: високосный');
     alert('Год високосный');
-   } else if(leapYear % 100 == 0) {
-    console.log('Год: не високосный');
-    alert('Год не високосный');
    } else {
     console.log('Год: не високосный');
     alert('Год не високосный');
